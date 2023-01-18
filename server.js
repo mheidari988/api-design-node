5a4d4cbaeb1fdc32fb5e19e6d209fd447f87ebfd
Reza Heidari <reza.heidari@amplicade.com>
2023-01-18 18:53:14 +0300

    wireup server

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+const express = require('express');
+const fs = require('fs');
+const app = express();
+var jsonData = { count: 12, message: 'hey' };
 
-var jsonData = {count: 12, message: 'hey'};
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html', (err) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+    })
+});
+
+app.get('/fs', (req, res) => {
+    fs.readFile('index.html', (err, buffer) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        res.setHeader('Content-Type', 'text/html');
+        res.send(buffer.toString());
+    });
+});
+
+app.get('/data', (req, res) => {
+    res.json(jsonData);
+})
+
+app.listen(3000, () => {
+    console.log('listening on port http://localhost:3000');
+})
